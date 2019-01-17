5f27e525af6f5ecd3e73715497ee80af845409d7
Alexsandro Specht <alexsandrospecht@gmail.com>
2019-01-17 21:58:15 -0200

    Hystrix Manager and Actuators

--- circuit-breaker/bookstore-client/src/main/java/com/github/specht/bookstoreclient/controller/BookController.java
+++ circuit-breaker/bookstore-client/src/main/java/com/github/specht/bookstoreclient/controller/BookController.java
@@ -2,14 +2,12 @@ package com.github.specht.bookstoreclient.controller;
 
 import com.github.specht.bookstoreclient.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@EnableCircuitBreaker
 public class BookController {
 
     @Autowired
